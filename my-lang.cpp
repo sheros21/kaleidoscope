a617ef7bf080a21bb79f722ecc069c216dd63b60
Rosie <71781274+sheros21@users.noreply.github.com>
2023-09-08 11:54:24 +0800

    Update my-lang.cpp

--- my-lang.cpp
+++ my-lang.cpp
@@ -8,13 +8,13 @@
 
 
 // Forward declare the ExprAST class
-/// ExprAST - Base class for all expression nodes.
+// ExprAST - Base class for all expression nodes.
 class ExprAST {
 public:
   virtual ~ExprAST() = default;
 };
 
-/// NumberExprAST - Expression class for numeric literals like "1.0".
+// NumberExprAST - Expression class for numeric literals like "1.0".
 class NumberExprAST : public ExprAST {
   double Val;
 public:
